1dcc816d66a9f9b2d88ae67030995d841d1ac157
local-li <2939634393@qq.com>
2022-08-15 23:55:13 +0800

    清理无用引入

--- src/main/java/me/codeleep/jsondiff/handle/object/AbstractObjectHandle.java
+++ src/main/java/me/codeleep/jsondiff/handle/object/AbstractObjectHandle.java
@@ -1,21 +1,10 @@
 package me.codeleep.jsondiff.handle.object;
 
-import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import me.codeleep.jsondiff.handle.AbstractDiffHandle;
-import me.codeleep.jsondiff.handle.Handle;
-import me.codeleep.jsondiff.handle.HandleExampleFactory;
 import me.codeleep.jsondiff.handle.RunTimeDataFactory;
-import me.codeleep.jsondiff.handle.array.AbstractArrayHandle;
-import me.codeleep.jsondiff.handle.array.SimpleArrayHandle;
-import me.codeleep.jsondiff.handle.object.ObjectHandle;
-import me.codeleep.jsondiff.model.Defects;
 import me.codeleep.jsondiff.model.JsonCompareResult;
-import me.codeleep.jsondiff.utils.JsonDiffUtil;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Map;
 import java.util.Set;
 
 /**
